138d2d78f1e07e81a43b3fcc7dcded558af8ccc5
Evgeny Luvsandugar <evgeny@luvsandugar.com>
2020-07-20 13:46:26 +0300

    Add cpu and memory

--- variables.tf
+++ variables.tf
@@ -20,6 +20,18 @@ variable "container_definition" {
   default     = null
 }
 
+variable "container_cpu" {
+  # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/AWS_Fargate.html#fargate-task-defs
+  description = "(Optional) The number of cpu units to reserve for the container. This is optional for tasks using Fargate launch type and the total amount of container_cpu of all containers in a task will need to be lower than the task-level cpu value"
+  default     = 1024 # 1 vCPU
+}
+
+variable "container_memory" {
+  # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/AWS_Fargate.html#fargate-task-defs
+  description = "(Optional) The amount of memory (in MiB) to allow the container to use. This is a hard limit, if the container attempts to exceed the container_memory, the container is killed. This field is optional for Fargate launch type and the total amount of container_memory of all containers in a task will need to be lower than the task memory value"
+  default     = 8192 # 8 GB
+}
+
 variable "placement_constraints" {
   description = "(Optional) A set of placement constraints rules that are taken into consideration during task placement. Maximum number of placement_constraints is 10. This is a list of maps, where each map should contain \"type\" and \"expression\""
   type        = list
